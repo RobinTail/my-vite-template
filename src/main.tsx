bb5f3bc33b4e70958c778dcfce18efa795474639
Robin Tail <robin_tail@me.com>
2024-03-06 22:58:32 +0100

    Fix import of Alert.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Alert } from "@mui/material";
+import Alert from "@mui/material/Alert";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
