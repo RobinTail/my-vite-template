c4737171b8cbadfe2b4f0894e4a7f0cd4a300c48
Anna Bocharova <robin_tail@me.com>
2024-07-28 21:56:52 +0200

    Replacing jsdom with happy-dom. (#2)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
+    environmentOptions: {
+      happyDOM: {
+        width: 1920,
+        height: 1024,
+        settings: {
+          navigator: { userAgent: "Mozilla/5.0 HappyDOM/jsdom/compatible" },
+        },
+      },
+    },
   },
 });
