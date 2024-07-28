470f796e394f9b48748057dba9ebb8339bc06795
Robin Tail <robin_tail@me.com>
2024-07-28 22:08:48 +0200

    Using ESLint flat config.

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from "globals";
+import jsPlugin from "@eslint/js";
+import tsPlugin from "typescript-eslint";
+import prettierOverrides from "eslint-config-prettier";
+import prettierRules from "eslint-plugin-prettier/recommended";
+import hooksPlugin from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    languageOptions: { globals: { ...globals.node, ...globals.browser } },
+    plugins: {
+      "react-hooks": hooksPlugin,
+    },
+  },
+  jsPlugin.configs.recommended,
+  ...tsPlugin.configs.recommended,
+  prettierOverrides,
+  prettierRules,
+  // Things to turn off globally
+  { ignores: ["dist/"] },
+  {
+    rules: {
+      "no-empty": ["error", { allowEmptyCatch: true }],
+      "no-empty-pattern": ["error", { allowObjectPatternsAsParameters: true }],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+];
